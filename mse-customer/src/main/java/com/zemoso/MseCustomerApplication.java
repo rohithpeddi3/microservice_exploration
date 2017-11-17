7357f3ac6736482e25e94acdce8742d5413c1de6
Rohith Peddi <rohith.p@zemosolabs.com>
2017-11-17 13:12:20 +0530

    Customer Microservice Setup

---
+++ mse-customer/src/main/java/com/zemoso/MseCustomerApplication.java
@@ -0,0 +1,12 @@
+package com.zemoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MseCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MseCustomerApplication.class, args);
+	}
+}

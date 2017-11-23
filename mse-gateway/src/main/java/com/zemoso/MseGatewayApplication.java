64c705cc821f1596d4b9c00c5fba72b70973df47
Varun Vishwanadha <varun@zemosolabs.com>
2017-11-23 17:23:27 +0530

    service registry module

---
+++ mse-gateway/src/main/java/com/zemoso/MseGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.zemoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MseGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MseGatewayApplication.class, args);
+	}
+}
